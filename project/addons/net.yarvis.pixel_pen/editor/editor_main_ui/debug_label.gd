8c5ac0dc0585985eba56f955ec276c1fbd0169b1
Bayu Santoso Widodo <110602582+bayu-sw@users.noreply.github.com>
2024-08-14 15:55:17 +0700

    patch v1.1.1 (#23)

--- project/addons/net.yarvis.pixel_pen/editor/editor_main_ui/debug_label.gd
+++ project/addons/net.yarvis.pixel_pen/editor/editor_main_ui/debug_label.gd
@@ -3,6 +3,7 @@ extends Label
 
 
 var data_debug : Dictionary = {}
+var version : String
 
 
 func _ready():
@@ -15,11 +16,22 @@ func _ready():
 			)
 
 
+func _enter_tree():
+	if Engine.is_editor_hint():
+		var cfg := ConfigFile.new()
+		if cfg.load("res://addons/net.yarvis.pixel_pen/plugin.cfg") == OK:
+			version = cfg.get_value("plugin", "version")
+	else:
+		version = ProjectSettings.get_setting("application/config/version")
+
+
 func _process(_delta):
 	if not PixelPen.state.need_connection(get_window()):
 		text = ""
 		return
-	var txt = str("FPS: ", Engine.get_frames_per_second())
+	var txt = str(
+		str("PixelPen ", version, "\n") if version != "" else "", "FPS: ",
+		Engine.get_frames_per_second())
 	data_debug["Memory"] = str(Performance.get_monitor(Performance.MEMORY_STATIC)/1000000, "/",
 			Performance.get_monitor(Performance.MEMORY_STATIC_MAX)/1000000, " mb")
 	for key in data_debug.keys():
